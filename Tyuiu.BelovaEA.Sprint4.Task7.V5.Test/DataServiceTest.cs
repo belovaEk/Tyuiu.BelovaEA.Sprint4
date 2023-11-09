8e4ccac54f881aaf8198e2fe6943ab78040e1a8f
Tyuiu.BelovaEA@outlook.com <belova.ek0@gmail,com>
2023-11-09 10:15:51 +0500

    Task7

---
+++ Tyuiu.BelovaEA.Sprint4.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BelovaEA.Sprint4.Task7.V5.Lib;
+
+namespace Tyuiu.BelovaEA.Sprint4.Task7.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new DataService();
+            string a = "123456";
+            int res = ds.Calculate(2,3, a);
+            Assert.AreEqual(3,res);
+        }
+    }
+}
---
+++ Tyuiu.BelovaEA.Sprint4.Task7.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BelovaEA.Sprint4.Task7.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BelovaEA.Sprint4.Task7.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fd7553c-4bfd-4538-9c17-0372ea32d0ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
